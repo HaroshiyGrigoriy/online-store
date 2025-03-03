b1a8a7086444a6df56cede6d7047a0e3a31379f3
guruw <guruwolf8@gmail.com>
2025-03-03 10:20:19 +0300

    Create project,for my repositories

---
+++ src/main/java/com/grishiya/alya_avito/AlyaAvitoApplication.java
@@ -0,0 +1,13 @@
+package com.grishiya.alya_avito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlyaAvitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlyaAvitoApplication.class, args);
+	}
+
+}
